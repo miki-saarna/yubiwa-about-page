f2334c620115501bfa3ed63cca9fcedc5df855e2
Mikito Saarna <mikitosaarna@gmail.com>
2021-11-02 19:32:29 -0500

    restore to version of script before attempting to remove event listeners for desktop view

--- script.js
+++ script.js
@@ -1,141 +1,116 @@
-// const result = window.matchMedia("(max-width: 700px)");
-// console.log(result.matches)
-
-// window.addEventListener('resize', isSmallScreen);
-
-
-
 const isSmallScreen = () => {
   return window.matchMedia("(max-width: 1024px)").matches;
 }
 
-window.addEventListener('resize', () => {
-  
-  // get our elements
-  const slider = document.querySelector('.slider-container'),
-    slides = Array.from(document.querySelectorAll('.slide'))
+console.log(isSmallScreen())
 
-  // set up our state
-  let isDragging = false,
-    startPos = 0,
-    currentTranslate = 0,
-    prevTranslate = 0,
-    animationID,
-    currentIndex = 0
-  
-  isSmallScreen;
+// get our elements
+const slider = document.querySelector('.slider-container'),
+  slides = Array.from(document.querySelectorAll('.slide'))
 
-  if(isSmallScreen()) {
+// set up our state
+let isDragging = false,
+  startPos = 0,
+  currentTranslate = 0,
+  prevTranslate = 0,
+  animationID,
+  currentIndex = 0
 
-  // add our event listeners
-  slides.forEach((slide, index) => {
-    const slideImage = slide.querySelector('img')
-    // disable default image drag
-    slideImage.addEventListener('dragstart', (e) => e.preventDefault())
-    // touch events
-    slide.addEventListener('touchstart', touchStart(index))
-    slide.addEventListener('touchend', touchEnd)
-    slide.addEventListener('touchmove', touchMove)
-    // mouse events
-    slide.addEventListener('mousedown', touchStart(index))
-    slide.addEventListener('mouseup', touchEnd)
-    slide.addEventListener('mousemove', touchMove)
-    slide.addEventListener('mouseleave', touchEnd)
-  })
-
-  // make responsive to viewport changes
-  window.addEventListener('resize', setPositionByIndex)
-
-  // prevent menu popup on long press
-  window.oncontextmenu = function (event) {
-    event.preventDefault()
-    event.stopPropagation()
-    return false
-  }
-
-  function getPositionX(event) {
-    return event.type.includes('mouse') ? event.pageX : event.touches[0].clientX
-  }
-
-  // use a HOF so we have index in a closure
-  function touchStart(index) {
-    return function (event) {
-      currentIndex = index
-      startPos = getPositionX(event)
-      isDragging = true
-      animationID = requestAnimationFrame(animation)
-      slider.classList.add('grabbing')
-    }
-  }
-
-  function touchMove(event) {
-    if (isDragging) {
-      const currentPosition = getPositionX(event)
-      currentTranslate = prevTranslate + currentPosition - startPos
-    }
-  }
-
-  function touchEnd() {
-    cancelAnimationFrame(animationID)
-    isDragging = false
-    const movedBy = currentTranslate - prevTranslate
-
-    // if moved enough negative then snap to next slide if there is one
-    if (movedBy < -100 && currentIndex < slides.length - 1) {
-        currentIndex += 1;
-        // for menu selected bar   
-        const selected = menuSelected(currentIndex);
-        selected.classList.add("filled");
-        // for menu selected bar (prev)  
-        const prevIndex = currentIndex - 1;
-        const prevSelected = menuSelected(prevIndex);
-        prevSelected.classList.remove("filled");
-    }
-    // if moved enough positive then snap to previous slide if there is one
-    if (movedBy > 100 && currentIndex > 0) {
-        currentIndex -= 1;
-        // for menu selected bar   
-        const selected = menuSelected(currentIndex);
-        selected.classList.add("filled");
-        // for menu selected bar (prev)  
-        const prevIndex = currentIndex + 1;
-        const prevSelected = menuSelected(prevIndex);
-        prevSelected.classList.remove("filled");
-    }
-    setPositionByIndex()
-
-    slider.classList.remove('grabbing')
-  }
-
-  function animation() {
-    setSliderPosition()
-    if (isDragging) requestAnimationFrame(animation)
-  }
-
-  function setPositionByIndex() {
-    currentTranslate = currentIndex * -window.innerWidth
-    prevTranslate = currentTranslate
-    setSliderPosition()
-  }
-
-  function setSliderPosition() {
-    slider.style.transform = `translateX(${currentTranslate}px)`
-  }
-
-  function menuSelected(index) {
-      return document.querySelector(`.imageSelected${index + 1}`);
-  }
-
-
-  } else {
-    isDragging = false,
-    startPos = 0,
-    currentTranslate = 0,
-    prevTranslate = 0,
-    animationID,
-    currentIndex = 0;
-    slider.classList.remove('grabbing')
-  }
+// add our event listeners
+slides.forEach((slide, index) => {
+  const slideImage = slide.querySelector('img')
+  // disable default image drag
+  slideImage.addEventListener('dragstart', (e) => e.preventDefault())
+  // touch events
+  slide.addEventListener('touchstart', touchStart(index))
+  slide.addEventListener('touchend', touchEnd)
+  slide.addEventListener('touchmove', touchMove)
+  // mouse events
+  slide.addEventListener('mousedown', touchStart(index))
+  slide.addEventListener('mouseup', touchEnd)
+  slide.addEventListener('mousemove', touchMove)
+  slide.addEventListener('mouseleave', touchEnd)
 })
 
-  // window.addEventListener('resize', isSmallScreen);
+// make responsive to viewport changes
+window.addEventListener('resize', setPositionByIndex)
+
+// prevent menu popup on long press
+window.oncontextmenu = function (event) {
+  event.preventDefault()
+  event.stopPropagation()
+  return false
+}
+
+function getPositionX(event) {
+  return event.type.includes('mouse') ? event.pageX : event.touches[0].clientX
+}
+
+// use a HOF so we have index in a closure
+function touchStart(index) {
+  return function (event) {
+    currentIndex = index
+    startPos = getPositionX(event)
+    isDragging = true
+    animationID = requestAnimationFrame(animation)
+    slider.classList.add('grabbing')
+  }
+}
+
+function touchMove(event) {
+  if (isDragging) {
+    const currentPosition = getPositionX(event)
+    currentTranslate = prevTranslate + currentPosition - startPos
+  }
+}
+
+function touchEnd() {
+  cancelAnimationFrame(animationID)
+  isDragging = false
+  const movedBy = currentTranslate - prevTranslate
+
+  // if moved enough negative then snap to next slide if there is one
+  if (movedBy < -100 && currentIndex < slides.length - 1) {
+      currentIndex += 1;
+      // for menu selected bar   
+      const selected = menuSelected(currentIndex);
+      selected.classList.add("filled");
+      // for menu selected bar (prev)  
+      const prevIndex = currentIndex - 1;
+      const prevSelected = menuSelected(prevIndex);
+      prevSelected.classList.remove("filled");
+  }
+  // if moved enough positive then snap to previous slide if there is one
+  if (movedBy > 100 && currentIndex > 0) {
+      currentIndex -= 1;
+      // for menu selected bar   
+      const selected = menuSelected(currentIndex);
+      selected.classList.add("filled");
+      // for menu selected bar (prev)  
+      const prevIndex = currentIndex + 1;
+      const prevSelected = menuSelected(prevIndex);
+      prevSelected.classList.remove("filled");
+  }
+  setPositionByIndex()
+
+  slider.classList.remove('grabbing')
+}
+
+function animation() {
+  setSliderPosition()
+  if (isDragging) requestAnimationFrame(animation)
+}
+
+function setPositionByIndex() {
+  currentTranslate = currentIndex * -window.innerWidth
+  prevTranslate = currentTranslate
+  setSliderPosition()
+}
+
+function setSliderPosition() {
+  slider.style.transform = `translateX(${currentTranslate}px)`
+}
+
+function menuSelected(index) {
+    return document.querySelector(`.imageSelected${index + 1}`);
+}
